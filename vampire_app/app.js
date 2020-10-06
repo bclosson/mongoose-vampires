6d1b23e90e38e7f992be7b5f96d9332ad1ace8c8
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-10-06 08:36:59 -0700

    Commit 5 added vampires

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -11,21 +11,70 @@ const vampires = require('./populateVampires');
 
 /////////////////////////////////////////////////
 //Write your answers to add, query, update, remove, and Hungry for More below.
+const newVampires = [{
+        name: 'Geraldine Pertaka',
+        hair_color: 'white',
+        eye_color: 'green',
+        dob: new Date(367, 4, 22, 6, 55),
+        loves: ['pain','heartache'],
+        location: 'Seattle, Washington, US',
+        gender: 'f',
+        victims: 1000,
+    },
+    {
+        name: 'Victor Frankenstein',
+        hair_color: 'blond',
+        eye_color: 'black',
+        dob: new Date(1794, 5, 19, 9, 22),
+        loves: ['hunchbacks', 'brains'],
+        location: 'Transylvania, Romania',
+        gender: 'm',
+        victims: 420,
+    },
+    {
+        name: 'Santanico Pandemonium',
+        hair_color: 'red',
+        eye_color: 'yellow',
+        dob: new Date(1922, 9, 19, 22, 3),
+        loves: ['hard life', 'mischief'],
+        location: 'El Rey, Mexico',
+        gender: 'f',
+        victims: 324,
+    },
+    {
+        name: 'Lord Ruthven',
+        hair_color: 'brown',
+        eye_color: 'silver',
+        dob: new Date(1734, 3, 24, 17, 8),
+        loves: ['blood', 'bats', 'fresh kills'],
+        location: 'Cambridge, England',
+        gender: 'm',
+        victims: 937,
+    },
+];
 
+const newVamps = newVampires;
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
-db.Vampire.insertMany(vampires, (err, addedVampires) => {
+// db.Vampire.insertMany(vampires, (err, addedVampires) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(addedVampires);
+//     }
+//     process.exit(); 
+// });
+// ### Add some new vampire data
+db.Vampire.insertMany(newVamps, (err, addedVamps) => {
     if (err) {
         console.log(err);
     } else {
-        console.log(addedVampires);
+        console.log(addedVamps);
     }
-    process.exit(); 
+    process.exit();
 });
-// ### Add some new vampire data
-
 /////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
